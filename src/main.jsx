8df4c39271b3941cf90af3dd0d10de03a47dd19a
Murli Manohar <murlimanohar1212@gmail.com>
2025-06-03 10:17:32 +0530

    main.jsx with latest changes

--- src/main.jsx
+++ src/main.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  
     <RouterProvider router={router} />
-  </HashRouter>,
+  
 )
